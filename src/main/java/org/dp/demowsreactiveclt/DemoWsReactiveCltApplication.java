f810b9cb48ab5806647c59d9c531f852a972cded
dvperv <dvperv@gmail.com>
2023-03-20 18:03:12 +0300

    Initial commit

--- src/main/java/org/dp/demowsreactiveclt/DemoWsReactiveCltApplication.java
+++ src/main/java/org/dp/demowsreactiveclt/DemoWsReactiveCltApplication.java
@@ -1,33 +1,32 @@
 package org.dp.demowsreactiveclt;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
-import org.springframework.web.reactive.socket.client.WebSocketClient;
+import reactor.core.publisher.Mono;
 
 import java.net.URI;
-
 @SpringBootApplication
 public class DemoWsReactiveCltApplication {
-	private final WebSocketClient client;
+    Logger logger = LoggerFactory.getLogger(DemoWsReactiveCltApplication.class);
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWsReactiveCltApplication.class, args);
+    }
 
-	public DemoWsReactiveCltApplication(WebSocketClient client) {
-		this.client = client;
-	}
+    @Bean
+    public void wsClientHandler() {
+        URI url = URI.create("ws://localhost:8080/out");
+        ReactorNettyWebSocketClient client = new ReactorNettyWebSocketClient();
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoWsReactiveCltApplication.class, args);
-	}
-
-	@Bean
-	public void wsClientHandler(){
-		URI url = URI.create("ws://localhost:8080/out");
-		client.execute(url, session ->
-				session
-					.receive()
-					.doOnNext(System.out::println)
-					.then());
-	}
+        client.execute(url, session -> session.receive()
+                .doOnNext(webSocketMessage -> {
+                    System.out.println(webSocketMessage.getPayloadAsText());
+                })
+                .then()
+        );
+    }
 
 }
